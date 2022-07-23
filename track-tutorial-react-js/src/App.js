13bbb6768de69542ea86bb403e23ee3ee2b89d89
monkukui <monkukui1953@gmail.com>
2022-07-23 17:24:38 +0900

    Add template of react-js

--- track-tutorial-react-js/src/App.js
+++ track-tutorial-react-js/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TrackButton from "./components/TrackButton";
+import TrackText from "./components/TrackText";
 
 function App() {
+  const [text, setText] = useState("");
+  const onClick = () => {
+    setText("hello track");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      textAlign: "center",
+    }}>
+      <TrackButton onClick={onClick} />
+      <TrackText text={text}/>
     </div>
   );
 }
